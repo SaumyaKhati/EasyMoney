90d4d46a1b51d26d1bf4193535a91e63de2b3c1a
SaumyaK <saumyakhati1@gmail.com>
2021-11-20 20:59:45 -0500

    Add changes to views.py

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__)
@@ -12,5 +12,12 @@ def home():
 @views.route('/add', methods=['GET', 'POST'])
 @login_required
 def add_item():
+    if request.method == 'POST':
+        date = request.form.get('date')
+        category = request.form.get('category')
+        item = request.form.get('item')
+        price = request.form.get('price')
+
+        
     return render_template('add_item.html', user=current_user)
 
