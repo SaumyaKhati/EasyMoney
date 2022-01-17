cc77113421b99b4b78e4f7015f70d41647b61f84
SaumyaK <saumyakhati1@gmail.com>
2022-01-17 17:11:46 -0500

    Set debug env. to false for app.

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from website import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True) # Everytime we change the python files, it'll rerun the webserver.
+    app.run(debug=False) # Everytime we change the python files, it'll rerun the webserver.
